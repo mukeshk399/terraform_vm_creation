b65756b92d147de0919665e3b616832c019e7c4c
mukeshk399 <61782972+mukeshk399@users.noreply.github.com>
2023-07-19 20:07:13 +0530

    Add files via upload

---
+++ sync_db.ps1
@@ -0,0 +1,11 @@
+﻿# add path for SQLPackage.exe
+IF (-not ($env:Path).Contains( "C:\Program Files\Microsoft SQL Server\160\DAC\bin"))
+{ $env:path = $env:path + ";C:\Program Files\Microsoft SQL Server\160\DAC\bin;" }
+
+sqlpackage.exe /a:extract /of:true /scs:"server=.\mydb_dev;database=db_source;trusted_connection=true" /tf:"C:\test\db_source.dacpac";
+
+sqlpackage.exe /a:deployreport /op:"c:\test\report.xml" /of:True /sf:"C:\test\db_source.dacpac" /tcs:"server=.\sql2016; database=db_target;trusted_connection=True" 
+
+[xml]$x = gc -Path "c:\test\report.xml";
+$x.DeploymentReport.Operations.Operation |
+% -Begin {$a=@();} -process {$name = $_.name; $_.Item | %  {$r = New-Object PSObject -Property @{Operation=$name; Value = $_.Value; Type = $_.Type} ; $a += $r;} }  -End {$a}
